c057c9c6cf5341b0ab1e7755d26d5030a17c373e
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2019-04-28 22:21:32 +0600

    welcome changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <a href="/todos">Todos</a>
+                    <a href="/todos">TODOSSSS</a>
                 </div>
 
                
