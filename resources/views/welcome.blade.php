56dcc28e88fef299afa013de674651e37ceffc8a
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2019-04-28 23:20:24 +0600

    Name Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <a href="/todos">TODOSSSS</a>
+                    <a href="/todos">TODOAPP</a>
                 </div>
 
                
